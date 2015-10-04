cb6f9f52a828c77f78518f583659d454c335a6ef
Muriel Silveira <murielkong@gmail.com>
2015-10-04 19:47:54 -0300

    Cat premiun

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -1,42 +1,87 @@
-'use strict'
+'use strict';
 
-window.addEventListener('load', function() {
-    var cats = [
-            {name: 'Smally Cat', img: 'img/smally-cat.jpg', counter: 0},
-            {name: 'Stair Cat', img: 'img/stair-cat.jpg', counter: 0},
-            {name: 'Sleepy Cat', img: 'img/sleepy-cat.jpg', counter: 0},
-        ],
-        listContainer = document.getElementById('list-container'),
-        catContainer = document.getElementById('cat-container')
+(function(){
 
-    cats.forEach(function(cat, index) {
-        var item = document.createElement('li')
-        item.innerHTML = cat.name
-        item.classList.add('list-item')
-        item.setAttribute('data-index', index)
-        item.addEventListener('click', showCat)
-        listContainer.appendChild(item)
-    })
+    var model = {
+        init: function() {
+            this.cats = [
+                {name: 'Smally Cat', img: 'img/smally-cat.jpg', counter: 0},
+                {name: 'Stair Cat', img: 'img/stair-cat.jpg', counter: 0},
+                {name: 'Sleepy Cat', img: 'img/sleepy-cat.jpg', counter: 0},
+            ]
+        },
+        getAllCats: function() {
+            return this.cats
+        },
+        getCat: function(index) {
+            return this.cats[index]
+        }
+    },
 
-    function showCat (event) {
-        var index = parseInt(event.target.getAttribute('data-index')),
-            cat = cats[index],
-            catCounter,
-            catImg,
-            catsHtml =
-                '<div>' +
-                '   <span id="cat-name">' + cat.name + ' - </span>' +
-                '   <span id="cat-counter">' + cat.counter + '</span>' +
-                '   <img id="cat-img" src="' + cat.img + '" alt="Click me <3">' +
-                '</div>'
+    octopus = {
+        init: function() {
+            model.init()
+            catView.init()
+            listView.init()
+        },
+        getAllCats: function() {
+            return model.getAllCats()
+        },
+        getCat: function(index) {
+            return model.getCat(index)
+        }
+    },
 
-        catContainer.innerHTML = catsHtml
+    listView = {
+        init: function() {
+            this.listContainer = document.getElementById('list-container')
+            this.listCats()
+        },
+        listCats: function() {
+            var cats = octopus.getAllCats(),
+                self = this
+            cats.forEach(function(cat, index) {
+                var item = document.createElement('li')
+                item.innerHTML = cat.name
+                item.classList.add('list-item')
+                item.setAttribute('data-index', index)
+                item.addEventListener('click', self.showCat)
+                self.listContainer.appendChild(item)
+            })
+        },
+        showCat: function(event) {
+            var index = parseInt(event.target.getAttribute('data-index'))
+            catView.showCat(index)
+        }
+    },
 
-        catCounter = document.getElementById('cat-counter')
-        catImg = document.getElementById('cat-img')
+    catView = {
+        init: function() {
+            this.catContainer = document.getElementById('cat-container')
+        },
+        showCat: function(index) {
+            var cat = octopus.getCat(index),
+                catCounter,
+                catImg,
+                catsHtml =
+                    '<div>' +
+                    '   <span id="cat-name">' + cat.name + ' - </span>' +
+                    '   <span id="cat-counter">' + cat.counter + '</span>' +
+                    '   <img id="cat-img" src="' + cat.img + '" alt="Click me <3">' +
+                    '</div>'
 
-        catImg.addEventListener('click', function() {
-            catCounter.innerHTML = ++cat.counter
-        })
+            this.catContainer.innerHTML = catsHtml
+
+            catCounter = document.getElementById('cat-counter')
+            catImg = document.getElementById('cat-img')
+
+            catImg.addEventListener('click', function() {
+                catCounter.innerHTML = ++cat.counter
+            })
+        }
     }
-})
+
+    window.addEventListener('load', function() {
+        octopus.init()
+    })
+})()
