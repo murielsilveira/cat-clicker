e207473610dfa088dc6d134d981199dc6939a1fd
Muriel Silveira <murielkong@gmail.com>
2015-10-05 21:36:58 -0300

    Refactor to use a current cat on model

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -3,42 +3,54 @@
 (function(){
 
     var model = {
+        currentCat: null,
         init: function() {
             this.cats = [
                 {name: 'Smally Cat', img: 'img/smally-cat.jpg', counter: 0},
                 {name: 'Stair Cat', img: 'img/stair-cat.jpg', counter: 0},
                 {name: 'Sleepy Cat', img: 'img/sleepy-cat.jpg', counter: 0},
                 {name: 'Doido Cat', img: 'img/doido-cat.jpg', counter: 0},
-            ]
+            ].sort(this._catOrderCompare)
         },
         getAllCats: function() {
-            return this.cats
+            return this.cats.sort(model._catOrderCompare)
         },
         getCat: function(index) {
             return this.cats[index]
+        },
+        getCurrentCat: function() {
+            return this.currentCat
+        },
+        _catOrderCompare: function(a, b) {
+            return a.name.toLowerCase() > b.name.toLowerCase()
         }
     },
 
     octopus = {
         init: function() {
             model.init()
+            model.currentCat = model.getCat(0)
+            catListView.init()
             catView.init()
-            listView.init()
         },
         getAllCats: function() {
             return model.getAllCats()
         },
-        getCat: function(index) {
-            return model.getCat(index)
+        getCurrentCat: function() {
+            return model.currentCat
+        },
+        setCurrentCat: function(index) {
+            model.currentCat = model.getCat(index)
+            catView.render()
         }
     },
 
-    listView = {
+    catListView = {
         init: function() {
             this.listContainer = document.getElementById('list-container')
-            this.listCats()
+            this.render()
         },
-        listCats: function() {
+        render: function() {
             var cats = octopus.getAllCats(),
                 self = this
             cats.forEach(function(cat, index) {
@@ -46,24 +58,28 @@
                 item.innerHTML = cat.name
                 item.classList.add('list-item')
                 item.setAttribute('data-index', index)
-                item.addEventListener('click', self.showCat)
+                item.addEventListener('click', self.showCatEvent)
                 self.listContainer.appendChild(item)
             })
         },
-        showCat: function(event) {
+        showCatEvent: function(event) {
             var index = parseInt(event.target.getAttribute('data-index'))
-            catView.showCat(index)
+            octopus.setCurrentCat(index)
         }
     },
 
     catView = {
         init: function() {
             this.catContainer = document.getElementById('cat-container')
+            this.render()
         },
-        showCat: function(index) {
-            var cat = octopus.getCat(index),
+        render: function() {
+            var cat = octopus.getCurrentCat(),
+                catsHtml,
                 catCounter,
-                catImg,
+                catImg
+
+            if (cat) {
                 catsHtml =
                     '<div>' +
                     '   <span id="cat-name">' + cat.name + ' - </span>' +
@@ -71,14 +87,17 @@
                     '   <img id="cat-img" src="' + cat.img + '" alt="Click me <3">' +
                     '</div>'
 
-            this.catContainer.innerHTML = catsHtml
+                this.catContainer.innerHTML = catsHtml
 
-            catCounter = document.getElementById('cat-counter')
-            catImg = document.getElementById('cat-img')
+                catCounter = document.getElementById('cat-counter')
+                catImg = document.getElementById('cat-img')
 
-            catImg.addEventListener('click', function() {
-                catCounter.innerHTML = ++cat.counter
-            })
+                catImg.addEventListener('click', function() {
+                    catCounter.innerHTML = ++cat.counter
+                })
+            } else {
+                this.catContainer.innerHTML = ''
+            }
         }
     }
 
