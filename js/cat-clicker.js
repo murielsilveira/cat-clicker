0442961e2bcc24c42db2f0f01232eb529f9cd423
Muriel Silveira <murielkong@gmail.com>
2016-03-16 01:03:02 -0300

    Cat clicker premium first version

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -18,8 +18,12 @@
         getCat: function(index) {
             return this.cats[index]
         },
-        getCurrentCat: function() {
-            return this.currentCat
+        saveCat: function(index, name, imgUrl, numClicks) {
+            this.cats[index] = {
+                name: name,
+                img: imgUrl,
+                counter: numClicks
+            }
         },
         _catOrderCompare: function(a, b) {
             return a.name.toLowerCase() > b.name.toLowerCase()
@@ -29,18 +33,26 @@
     octopus = {
         init: function() {
             model.init()
-            model.currentCat = model.getCat(0)
+            this.currentCatIndex = 0
             catListView.init()
             catView.init()
+            catAdminView.init()
         },
         getAllCats: function() {
             return model.getAllCats()
         },
         getCurrentCat: function() {
-            return model.currentCat
+            return model.getCat(this.currentCatIndex)
         },
         setCurrentCat: function(index) {
-            model.currentCat = model.getCat(index)
+            this.currentCatIndex = index
+            catAdminView.closeAdmin()
+            catView.render()
+        },
+        saveCat: function(name, imgUrl, numClicks) {
+            model.saveCat(this.currentCatIndex, name, imgUrl, numClicks)
+            catAdminView.closeAdmin()
+            catListView.render()
             catView.render()
         }
     },
@@ -53,6 +65,7 @@
         render: function() {
             var cats = octopus.getAllCats(),
                 self = this
+            self.listContainer.innerHTML = ''
             cats.forEach(function(cat, index) {
                 var item = document.createElement('li')
                 item.innerHTML = cat.name
@@ -96,6 +109,61 @@
             } else {
                 this.catContainer.innerHTML = ''
             }
+        }
+    },
+
+    catAdminView = {
+        init: function() {
+            this.adminContainer = document.getElementById('cat-admin')
+            this.isOpen = false
+            this.render()
+        },
+        render: function() {
+            this.adminContainer.innerHTML = '<button id="btn-admin">Admin</button>'
+
+            document.getElementById('btn-admin')
+                .addEventListener('click', this.openAdmin.bind(this))
+        },
+        openAdmin: function() {
+            if (this.isOpen) return
+            
+            var cat = octopus.getCurrentCat()
+            var form = document.createElement('form')
+            form.id = 'form-admin'
+            form.innerHTML = '<label for="name">Name</label>' +
+                             '<input name="name" value="' + cat.name + '"/>' +
+                             '<label for="url">URL</label>' +
+                             '<input name="url" value="' + cat.img + '"/>' +
+                             '<label for="clicks">#Clicks</label>' +
+                             '<input name="clicks" value="' + cat.counter + '"/>' +
+                             '<div class="admin-actions">' +
+                             '<button id="btn-cancel" type="button">Cancel</button>' +
+                             '<button id="btn-save" type="button">Save</button>' +
+                             '</div>'
+            this.adminContainer.appendChild(form)
+            this.isOpen = true
+
+            document.getElementById('btn-admin').style.display = 'none'
+
+            document.getElementById('btn-cancel')
+                .addEventListener('click', this.closeAdmin.bind(this))
+            document.getElementById('btn-save')
+                .addEventListener('click', this.saveCat.bind(this))
+        },
+        closeAdmin: function() {
+            if (!this.isOpen) return
+            
+            this.isOpen = false
+            document.getElementById('btn-admin').style.display = 'block'
+            document.getElementById('form-admin').remove()
+        },
+        saveCat: function() {
+            if (!this.isOpen) return
+
+            var elements = document.getElementById('form-admin').elements
+            octopus.saveCat(elements.namedItem('name').value,
+                            elements.namedItem('url').value,
+                            elements.namedItem('clicks').value)
         }
     }
 
